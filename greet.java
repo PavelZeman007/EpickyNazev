7bbab9ee3a6fca1f0a0dd8ab36703fab1ef17309
PaulaKelley <paula.kelleyova@gmail.com>
2022-06-13 14:49:46 +0200

    a3.2

--- greet.java
+++ greet.java
@@ -2,6 +2,6 @@ public class Greet {
     
     public static void main(String[] args) {
         String name = "Paula";
-        System.out.println("Hello "+name);
+        System.out.println("Hello "+name+"!");
     }
 }
