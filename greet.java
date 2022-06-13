dbf7873fbf05481fc9889a03a53cb0f51246a2fd
Stepan Friedl <stepanfriedl13@gmail.com>
2022-06-13 14:13:34 +0200

    d3

--- greet.java
+++ greet.java
@@ -2,6 +2,6 @@ public class Greet {
     
     public static void main(String[] args) {
         String name = "Paula";
-        System.out.println("My name is "+name);
+        System.out.println("Hello "+name);
     }
 }
